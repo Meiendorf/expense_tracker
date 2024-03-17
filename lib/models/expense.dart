2f58d99e6f3b632b76a5b01c07fa20e561c75263
meiendorf <neo1110111@gmail.com>
2024-03-17 22:58:36 +0200

    Refactor imports and remove unused Expenses class

---
+++ lib/models/expense.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+const uuid = Uuid();
+
+class Expense {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
+  String get formattedDate => formatter.format(date);
+}
+
+enum Category {
+  food(Icons.lunch_dining),
+  travel(Icons.flight_takeoff),
+  leisure(Icons.movie),
+  work(Icons.work);
+
+  final IconData icon;
+
+  const Category(this.icon);
+}
