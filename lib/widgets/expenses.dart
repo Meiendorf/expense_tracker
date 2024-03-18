410e1e30ba0f45ca1719c47ede6c36162c54b537
meiendorf <neo1110111@gmail.com>
2024-03-18 22:37:34 +0200

    Add expense deletion functionality and undo option

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -38,8 +38,42 @@ class _ExpensesState extends State<Expenses> {
     setState(() => _registeredExpenses.add(expense));
   }
 
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () => setState(() {
+            _registeredExpenses.insert(expenseIndex, expense);
+          }),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onDismissed: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
@@ -56,9 +90,7 @@ class _ExpensesState extends State<Expenses> {
           mainAxisSize: MainAxisSize.min,
           children: [
             const Text('The chart'),
-            Expanded(
-              child: ExpensesList(expenses: _registeredExpenses),
-            ),
+            Expanded(child: mainContent),
           ],
         ),
       ),
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -4,17 +4,25 @@ import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
   final List<Expense> expenses;
+  final void Function(Expense expense) onDismissed;
 
   const ExpensesList({
     super.key,
     required this.expenses,
+    required this.onDismissed,
   });
 
   @override
   Widget build(context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onDismissed(expenses[index]);
+        },
+        child: ExpenseItem(expenses[index]),
+      ),
     );
   }
 }
